33919b82685d00b53eaa13ed3bc8fba18fb60567
wali21009 <muh.waliyuddin99@gmail.com>
2022-11-21 06:35:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/githubusers/User.java
@@ -0,0 +1,31 @@
+package com.example.githubusers;
+
+public class User {
+    private String photo;
+    private String name;
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
